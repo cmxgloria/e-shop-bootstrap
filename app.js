d794435fdbce35a5c99f36ba55807e6346f22381
gloria <cmxgloria@gmail.com>
2020-09-21 12:04:25 +1000

    Updates

---
+++ app.js
@@ -0,0 +1,16 @@
+// function ready() {
+//   const menu = document.querySelector(".menu");
+//   // const mainmenu = document.querySelector(".mainmenu");
+//   // // const menu = document.querySelector("nav-link");
+//   // const service = document.querySelector(".card");
+//   // const contact = document.querySelector("footer");
+
+//   const handleMenuFunc = function () {
+//     document.querySelector("body").innerHTML =
+//       '<image src="https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />';
+//   };
+
+//   menu.addEventListener("click", handleMenuFunc);
+// }
+
+// document.addEventListener("DOMContentLoaded", ready);
